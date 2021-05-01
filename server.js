1e1fd0eddc6f622578929f170f4d7b61deaaf36e
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-05-01 16:29:16 -0700

    done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const logger = require("morgan");
 const path = require("path");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
@@ -11,7 +11,40 @@ app.set('env', NODE_ENV);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger('tiny'));
+
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, "./colors.json")));
+
+const getColors = async (req, res, next) => {
+    try {
+        const colors = (function () {
+            var colors = [];
+            data.forEach(element => {
+                colors.push(element.color);
+            });
+            return colors;
+        })();
+        res.send(colors);
+    } catch (e) {
+        next(e);
+    }
+};
+
+const getSubcolors = async (req, res, next) => {
+    try {
+        const subcolors = data.find(color => color.color === req.params.color).subcolors;
+        if (!subcolors) {
+            const err = new Error('color not found');
+            err.status = 404;
+            throw err;
+        }
+        res.send(subcolors);
+    } catch (e) {
+        next(e);
+    }
+};
+
+app.route("/api/colors/:color").get(getSubcolors);
+app.route("/api/colors").get(getColors);
 
 app.use((req, res, next) => {
     const err = new Error(`${req.method} ${req.url} not found`);
